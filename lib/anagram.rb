f8cdc9836ef2b328c1cc4e0d006de480eaff2107
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-26 05:38:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ attr_accessor :anagrams
   def match(anagrams)
     anagrams.detect do |word|
       if word == !word
-        
+        wrd
       else word == word.split(" ")
         word
       end
