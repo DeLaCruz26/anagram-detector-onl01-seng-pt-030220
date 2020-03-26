57d07d3096255dfa15ab0f44cf6553e62678affd
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-26 05:58:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,7 @@
 # Your code goes here!
 class Anagram
-attr_accessor :anagrams
+  
+  attr_accessor :anagrams
 
   def initialize(anagrams)
     @anagrams = anagrams
@@ -9,6 +10,5 @@ attr_accessor :anagrams
   def match(anagrams)
     anagrams.select { |word| word.split("").sort == @anagrams.split("") .sort }
   end
-
 
 end
