d97dc4e81f15bd2c88831f4b09c6901219fc685b
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-26 05:36:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+attr_accessor :anagrams
+
+  def initialize(anagrams)
+    @anagrams = anagrams
+  end
+
+  def match(anagrams)
+    anagrams.detect do |word|
+      if word != word
+        
+      else word == word.split(" ")
+        word
+      end
+    end
+  end
+
+
+end
